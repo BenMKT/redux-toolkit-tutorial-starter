d2570d56c8ea857855e29b5c8f5cb8561ef6fa2e
Benson Kiai <benson.kiai86@gmail.com>
2023-08-24 05:43:51 +0300

    Use/dispatch reducers with event listeners and handlers

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,8 +1,11 @@
+import { clearCart } from '../features/cart/cartSlice';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+// import { clearCart } from '../features/cart/cartSlice';Its already been defined in cartSlice.js
 
 const CartContainer = () => {
-  const { cartItems, total, amount } = useSelector((store) => store.cart);
+    const { cartItems, total, amount } = useSelector((store) => store.cart);
+    const dispatch = useDispatch();
 
   if (amount < 1) {
     return (
@@ -29,10 +32,10 @@ const CartContainer = () => {
         <hr />
         <div className="cart-total">
           <h4>
-            total <span>${total}</span>
+            total <span>${total.toFixed(2)}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>clear cart</button>
       </footer>
     </section>
   );
